4d62b7199a14c3f344d39a4570c4ab8924604ce1
Martin Lippert <mlippert@pivotal.io>
2016-04-20 20:47:44 +0200

    first steps towards creating a model using artifical bean registry setup

--- services/org.springframework.ide.service.agent/src/main/java/org/springframework/ide/service/agent/internal/JavaConfigurationRoot.java
+++ services/org.springframework.ide.service.agent/src/main/java/org/springframework/ide/service/agent/internal/JavaConfigurationRoot.java
@@ -10,7 +10,15 @@
  *******************************************************************************/
 package org.springframework.ide.service.agent.internal;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.AnnotatedGenericBeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.BeanDefinitionHolder;
+import org.springframework.beans.factory.support.BeanDefinitionReaderUtils;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.annotation.AnnotationBeanNameGenerator;
+import org.springframework.context.annotation.AnnotationConfigUtils;
+import org.springframework.core.type.classreading.CachingMetadataReaderFactory;
+import org.springframework.core.type.classreading.MetadataReader;
 import org.springframework.ide.service.agent.BackChannel;
 import org.springframework.ide.service.agent.ConfigurationRoot;
 
@@ -22,6 +30,8 @@ public class JavaConfigurationRoot implements ConfigurationRoot {
 	private final String configElement;
 	private final BackChannel backChannel;
 	private final String projectName;
+	
+	private DefaultListableBeanFactory registry;
 
 	public JavaConfigurationRoot(String configElement, String projectName, BackChannel backChannel) {
 		this.configElement = configElement;
@@ -34,14 +44,36 @@ public class JavaConfigurationRoot implements ConfigurationRoot {
 		String rootClassName = configElement.substring(ConfigurationRoot.JAVA_CONFIG_ROOT_PREFIX.length());
 		if (rootClassName.length() > 0) {
 			try {
-				Class<?> rootClass = this.getClass().getClassLoader().loadClass(rootClassName);
-				SpringApplication application = new SpringApplication(rootClass);
-				application.setApplicationContextClass(ModelBuildingContextClass.class);
-				application.run(new String[] {});
-			} catch (ClassNotFoundException e) {
+//				Class<?> rootClass = this.getClass().getClassLoader().loadClass(rootClassName);
+//				SpringApplication application = new SpringApplication(rootClass);
+//				application.setApplicationContextClass(ModelBuildingContextClass.class);
+//				application.run(new String[] {});
+				
+				setupModelCreation(rootClassName);
+			} catch (Exception e) {
 				backChannel.sendException(e);
 			}
 		}
+	}
+	
+	protected void setupModelCreation(String rootClassName) throws Exception {
+		this.registry = new DefaultListableBeanFactory();
+
+		// register annotation processors
+		AnnotationConfigUtils.registerAnnotationConfigProcessors(registry);
+
+		// register root bean
+		CachingMetadataReaderFactory readerFactory = new CachingMetadataReaderFactory(this.getClass().getClassLoader());
+		MetadataReader metadataReader = readerFactory.getMetadataReader(rootClassName);
+		
+		AnnotatedGenericBeanDefinition beanDefinition = new AnnotatedGenericBeanDefinition(metadataReader.getAnnotationMetadata());
+		beanDefinition.setScope(BeanDefinition.SCOPE_SINGLETON);
+
+		AnnotationBeanNameGenerator nameGenerator = new AnnotationBeanNameGenerator();
+		String beanName = nameGenerator.generateBeanName(beanDefinition, this.registry);
+
+		BeanDefinitionHolder definitionHolder = new BeanDefinitionHolder(beanDefinition, beanName);
+		BeanDefinitionReaderUtils.registerBeanDefinition(definitionHolder, this.registry);
 	}
 	
 }
